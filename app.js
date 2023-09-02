4f279bd61a9693abfcff770d49c1ead3805eb39b
ArtCode101 <sanayut12.service@gmail.com>
2023-09-02 14:53:33 +0700

    add dbconnection to app

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3308,
+    user: 'root',
+    password: 'root',
+    database: 'todo'
+})
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 const app = express()
 app.use(express.json())
+
 const port = 3000
 
 app.listen(port,()=> {
